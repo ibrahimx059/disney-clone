9de3e385f2e7a542e3e1e72adb19b8b35e06dc8d
ibrahim <ibraheemmughal789@gmail.com>
2023-12-05 22:39:56 +0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Slider from "./Components/Slider";
+import ProductionHouse from "./Components/ProductionHouse";
+import GenreMovieList from "./Components/GenreMovieList";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="">
+      <Header />
+      <Slider/>
+      <ProductionHouse/>
+      <GenreMovieList/>
+    </div>
+  );
+}
+
+export default App;
